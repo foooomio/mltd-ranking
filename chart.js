8ba22c1a0a7d2e9f825914d8c7ba0a58bbe7825d
foooomio <foooomio@gmail.com>
2021-12-06 05:33:34 +0900

    First implementation

---
+++ chart.js
@@ -0,0 +1,80 @@
+const HOUR = 60 * 60 * 1000;
+
+Highcharts.setOptions({
+  lang: {
+    shortWeekdays: ['日', '月', '火', '水', '木', '金', '土'],
+    thousandsSep: ',',
+  },
+});
+
+export function drawChart(eventData, rankingLogs) {
+  Highcharts.chart('chart', {
+    title: {
+      text: eventData.name.replace(/[〜～]/, '<br>$&'),
+    },
+    subtitle: {
+      text: '情報元：<a href="https://matsurihi.me">matsurihi.me</a>',
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: {
+        minute: '%H:%M',
+        day: '%m/%d (%a)',
+      },
+      softMax: Date.parse(eventData.schedule.endDate),
+      plotLines: [{
+        value: Date.parse(eventData.schedule.boostBeginDate),
+        width: 2,
+        color: '#ffaaaa',
+        zIndex: 1,
+        label: {
+          text: '折り返し',
+          rotation: 0,
+          y: 16,
+        },
+      }],
+      gridLineWidth: 1,
+      tickInterval: 24 * HOUR,
+      minorTicks: true,
+      minorTickInterval: 3 * HOUR,
+      startOnTick: true,
+      endOnTick: true,
+      crosshair: true,
+    },
+    yAxis: {
+      title: undefined,
+    },
+    legend: {
+      enabled: false,
+    },
+    tooltip: {
+      animation: false,
+      shared: true,
+      xDateFormat: '%m/%d (%a) %H:%M',
+    },
+    time: {
+      timezoneOffset: -540,
+    },
+    chart: {
+      animation: false,
+      zoomType: 'x',
+    },
+    plotOptions: {
+      line: {
+        animation: false,
+        marker: {
+          enabled: false,
+          symbol: 'circle',
+          radius: 2,
+        },
+      },
+    },
+    series: rankingLogs.map(({ rank, data }) => ({
+      name: rank + '位',
+      data: [
+        [Date.parse(eventData.schedule.beginDate), 0],
+        ...data.map(({ score, summaryTime }) => [Date.parse(summaryTime), score]),
+      ],
+    })),
+  });
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+import princess, { range } from 'https://cdn.jsdelivr.net/gh/foooomio/mltd-tools@1.0.0/princess-client.js';
+import { drawChart } from './chart.js';
+
+const eventId = 212;
+const eventData = await princess.events.get({ eventId });
+const rankingLogs = await princess.rankings.logs({ eventId, type: 'eventPoint', ranks: range(1, 10) });
+
+drawChart(eventData, rankingLogs);
+
+document.addEventListener('touchmove', (e) => {
+  e.preventDefault();
+}, { passive: false });
